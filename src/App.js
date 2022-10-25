4ad5f2a88022345c6befdcb251012078b3bd45a1
George <Georgesimbe@icloud.com>
2022-10-25 14:30:17 +1030

    ReFactoring Map file

--- src/App.js
+++ src/App.js
@@ -1,46 +1,35 @@
 import './App.css';
 import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-// mapboxgl.workerUrl = "./mapbox-gl-csp-worker.js";
-import SearchBox from './SearchBox';
+import ReactMapGL from 'react-map-gl'
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line 
+import { NavigationControl } from 'react-map-gl';
 
 mapboxgl.accessToken = process.env.REACT_APP_TOKEN
 // console.log(process.env.REACT_APP_TOKEN)
 export default function App() {
-const mapContainer = useRef(null);
-const map = useRef(null);
-const [lng, setLng] = useState(138.63);
-const [lat, setLat] = useState(-34.8626);
-const [zoom, setZoom] = useState(10.45);
-
-useEffect(() => {
-if (map.current) return; // initialize map only once when application restarted
-  map.current = new mapboxgl.Map({
-  container: mapContainer.current,
-  style: 'mapbox://styles/mapbox/streets-v11',
-  center: [lng, lat],
-  zoom: zoom
-  });
-});
-
-useEffect(() => {
-  if (!map.current) return; 
-  map.current.on('move', () => {
-  setLng(map.current.getCenter().lng.toFixed(4));
-  setLat(map.current.getCenter().lat.toFixed(4));
-  setZoom(map.current.getZoom().toFixed(2));
-  });
-});
+const [viewPort,setViewPort] = useState({
+  longitude: 138.63,
+  latitude: -34.8626,
+  zoom: 10.45
+})
 
 return (
+<ReactMapGL
+{...viewPort}
+  mapboxAccessToken={process.env.REACT_APP_TOKEN}
+  mapStyle='mapbox://styles/mapbox/streets-v11'
+  onViewportChange={ viewPort => {
+    setViewPort(viewPort)
+  }}
+  // mapStyle='mapbox://styles/mapbox/streets-v11'
+  >
+    {/* <Marker 
+    latitude={lat}
+    longitude= {lng}
+    draggable
+    // onDragEnd={(e) => dispatch{ty}}
+    /> */}
+</ReactMapGL>
 
-<div ref={mapContainer} className="map-container">
-  <div/>  
-  {/* <SearchBox/> */}
-  <div className="sidebar">
-  Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-</div>
-
-</div>
 );
 }
