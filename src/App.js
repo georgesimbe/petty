5c445c013d656c826f7409cac1a00456f121a89a
George <Georgesimbe@icloud.com>
2022-10-25 19:12:59 +1030

    Slight Changes for map

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import React, { useRef, useEffect, useState } from 'react';
-import ReactMapGL from 'react-map-gl'
+import React,{useState } from 'react';
+import ReactMapGL, {Marker, FullscreenControl,GeolocateControl, NavigationControl} from 'react-map-gl'
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line 
-import { NavigationControl } from 'react-map-gl';
+// import { NavigationControl } from 'react-map-gl';
 
 mapboxgl.accessToken = process.env.REACT_APP_TOKEN
 // console.log(process.env.REACT_APP_TOKEN)
@@ -13,23 +13,30 @@ const [viewPort,setViewPort] = useState({
   zoom: 10.45
 })
 
-return (
-<ReactMapGL
-{...viewPort}
-  mapboxAccessToken={process.env.REACT_APP_TOKEN}
-  mapStyle='mapbox://styles/mapbox/streets-v11'
-  onViewportChange={ viewPort => {
-    setViewPort(viewPort)
-  }}
-  // mapStyle='mapbox://styles/mapbox/streets-v11'
-  >
-    {/* <Marker 
-    latitude={lat}
-    longitude= {lng}
-    draggable
-    // onDragEnd={(e) => dispatch{ty}}
-    /> */}
-</ReactMapGL>
+ const geolocateControlRef = React.useCallback((ref) => {
+    if (ref) {
+      // Activate as soon as the control is loaded
+      ref.trigger();
+    }
+  }, []);
 
+
+return (
+  <div>
+    <ReactMapGL
+    mapboxAccessToken={process.env.REACT_APP_TOKEN}
+    {...viewPort}
+      onMove={ viewPort => {
+        setViewPort(viewPort.viewPort)
+      }}
+      mapStyle='mapbox://styles/mapbox/streets-v11'
+      style={{width: "100vw", height: "100vh"}}
+      >
+        {/* <FullscreenControl /> */}
+        <GeolocateControl ref={geolocateControlRef}  position='bottom-right'/>
+        <NavigationControl position='bottom-left' />
+        {/* <Marker/> */}
+        </ReactMapGL>
+  </div>
 );
 }
