9e9494a4f157d38788762f50f22f95751a9457f4
George <Georgesimbe@icloud.com>
2022-10-26 09:48:30 +1030

    Backend Able to recieve data from api key

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,30 @@
 
+const  axios = require('axios');
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
-
+const dotenv = require('dotenv').config()
 const logger = require('./middlewares/logger')
-const sessions = require('./middlewares/sessions')
+
 
 app.listen(4567, function () {
-  console.log('Server is listening on port 4567')
+  console.log(`Server is listening on port ${PORT}`)
 })
+
 app.use(logger)
+
+
+let saFuelApi = axios.create({
+  baseURL: "https://fppdirectapi-prod.safuelpricinginformation.com.au/Subscriber/",
+  headers:{
+    Authorization: `FPDAPI SubscriberToken=${process.env.SAFUELAPI}`,
+    'Content-type': 'application/json',
+  }
+})
+
+app.get("/", function(req,res){
+  saFuelApi(`GetCountryFuelTypes?countryId=21`).then(({data}) => {
+    res.send(JSON.stringify(data))
+  })
+
+})
